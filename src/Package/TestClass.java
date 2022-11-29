dd1ff684e4b93549d6c1914261410df061633853
shrhkdgus888 <shrhkdgus888@naver.com>
2022-11-29 23:36:39 +0900

    This is Commit Message!!

--- src/Package/TestClass.java
+++ src/Package/TestClass.java
@@ -3,8 +3,8 @@ package Package;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello! This is Github Practicing!");
+		//Github Practice!
 	}
 
 }
